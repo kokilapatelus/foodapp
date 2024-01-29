bb3355ca30a6ca0a53bb15a245fe7af536f48f50
kokilapatelus <kokilapatelus@gmail.com>
2024-01-29 08:02:53 -0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { StarRatingComponent } from './compoenent/partial/star-rating/star-rating.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    StarRatingComponent,
+    
+    
+     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+   
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
